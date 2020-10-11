3106d1438a87c2c4ea7072a6b3b24f31480f3ba3
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-10-11 15:06:32 +0300

    Fibers and reconciliation

--- typedreact.ts
+++ typedreact.ts
@@ -1,7 +1,26 @@
-export type TIncProps = { [key: string]: unknown; children?: TChild[] };
-export type TProps = { [key: string]: unknown; children: TNode[] };
-export type TNode = { type: string; props: TProps };
-export type TChild = TNode | string;
+type RequestIdleCallbackHandle = any
+type RequestIdleCallbackOptions = {
+  timeout: number
+}
+type RequestIdleCallbackDeadline = {
+  readonly didTimeout: boolean
+  timeRemaining: () => number
+}
+
+declare global {
+  interface Window {
+    requestIdleCallback: (
+      callback: (deadline: RequestIdleCallbackDeadline) => void,
+      opts?: RequestIdleCallbackOptions,
+    ) => RequestIdleCallbackHandle
+    cancelIdleCallback: (handle: RequestIdleCallbackHandle) => void
+  }
+}
+
+export type TIncProps = {[key: string]: unknown; children?: TChild[]}
+export type TProps = {[key: string]: unknown; children: TNode[]}
+export type TNode = {type: string; props: TProps}
+export type TChild = TNode | string
 
 function createElement(
   type: string,
@@ -12,44 +31,218 @@ function createElement(
     type,
     props: {
       ...props,
-      children: children.map((child) =>
-        typeof child === "object" ? child : createTextElement(child)
+      children: children.map(child =>
+        typeof child === 'object' ? child : createTextElement(child),
       ),
     },
-  };
+  }
 }
 
 function createTextElement(text: string): TNode {
   return {
-    type: "TEXT_ELEMENT",
+    type: 'TEXT_ELEMENT',
     props: {
       nodeValue: text,
       children: [],
     },
-  };
+  }
 }
 
-function render(element: TNode, container: HTMLElement) {
+interface Fiber {
+  type?: string
+  dom: HTMLElement | Text
+  props: TProps
+  alternate?: Fiber
+  parent?: Fiber
+  sibling?: Fiber
+  child?: Fiber
+  effectTag?: string
+}
+
+function createDom(fiber) {
   const dom =
-    element.type == "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
+    fiber.type == 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(fiber.type)
 
-  const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+  const isProperty = key => key !== 'children'
+  Object.keys(fiber.props)
     .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name];
-    });
+    .forEach(name => {
+      dom[name] = fiber.props[name]
+    })
 
-  for (const child of element.props.children) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    render(child, dom);
+  return dom
+}
+
+const isEvent = key => key.startsWith('on')
+const isProperty = key => key !== 'children' && !isEvent(key)
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+
+function updateDom(dom, prevProps, nextProps) {
+  // Remove old or changed event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(key => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.removeEventListener(eventType, prevProps[name])
+    })
+
+  // Add event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2)
+
+      dom.addEventListener(eventType, nextProps[name])
+    })
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ''
+    })
+
+  // Add new properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+}
+
+function commitRoot() {
+  deletions.forEach(commitWork)
+  commitWork(wipRoot.child)
+  currentRoot = wipRoot
+  wipRoot = null
+}
+
+function commitWork(fiber: Fiber) {
+  if (!fiber) return
+
+  const domParent = fiber.parent.dom
+
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom != null) {
+    domParent.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom != null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+  } else if (fiber.effectTag === 'DELETION' && fiber.dom != null) {
+    domParent.removeChild(fiber.dom)
   }
 
-  container.appendChild(dom);
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
+function reconcileChildren(wipFiber: Fiber, elements) {
+  let index = 0
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index]
+
+    let newFiber: Fiber = null
+    const sameType = oldFiber && element && element.type == oldFiber.type
+
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: 'UPDATE',
+      }
+    }
+
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: 'PLACEMENT',
+      }
+    }
+
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = 'DELETION'
+      deletions.push(oldFiber)
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    index++
+  }
+}
+
+function render(element: TNode, container: HTMLElement | Text) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    alternate: currentRoot,
+  }
+
+  deletions = []
+  nextUnitOfWork = wipRoot
+}
+
+let nextUnitOfWork: Fiber = null
+let wipRoot: Fiber = null
+let currentRoot: Fiber = null
+let deletions: Fiber[] = []
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+
+  window.requestIdleCallback(workLoop)
+}
+
+// ts-ignore
+window.requestIdleCallback(workLoop)
+function performUnitOfWork(fiber: Fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  const elements = fiber.props.children
+  reconcileChildren(fiber, elements)
+
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber: Fiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+
+    nextFiber = nextFiber.parent
+  }
 }
 
 export default {
   createElement,
-};
+}
